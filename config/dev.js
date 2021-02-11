b26c5e17f1b85ffc88384f98f9cb47f03354a966
xiaodan cui <cxd@xiaodan-MacBook-Pro.local>
2021-02-10 23:43:54 -0800

    dfdf

---
+++ config/dev.js
@@ -0,0 +1,12 @@
+module.exports = {
+  googleClientID:
+    "949892276383-jer8aas8t8r9rrqlosp2vb8j77mvusf5.apps.googleusercontent.com",
+  googleClientSecret: "M1cXz0lwJNLJDKoEN2P8ZThG",
+  mongoURI:
+    "mongodb+srv://Xiaodan-Cui:Le1RZoWwlvZzw4Fx@emailly.tn9wc.mongodb.net/emailly?retryWrites=true&w=majority",
+  cookieKey: "retrterwfbfewdfvbg",
+  stripePublishableKey:
+    "pk_test_51IJMJiITp57eC9LnYXednuFjpWCEgCCyPDW2oiMcg2goxFAjUixKnL2SX1vHqxeIMtemld5p9ZuxWE3AAiFTUIB800YqMBAOC4",
+  stripeSecretKey:
+    "sk_test_51IJMJiITp57eC9LnWjfYt3iQnWvYZHDwXRxUD1gzv4GVlzfz4iMTLP1wtNImYzji8Q55tRmbYpydN6BqwjzAtmlY00xTVDdqOB",
+};
