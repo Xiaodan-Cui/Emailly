de0aa34682186e33fabb7020a9aa43949ec866ff
xiaodan cui <cxd@xiaodan-MacBook-Pro.local>
2021-02-11 09:47:21 -0800

    Added Config

--- index.js
+++ index.js
@@ -29,6 +29,19 @@ app.use(passport.session());
 require("./routes/authRoutes")(app);
 require("./routes/billingRoutes")(app);
 
+if (process.env.NODE_ENV === "production") {
+  //Express will serve up production assets
+  //like main.js,main css
+
+  app.use(express.static("client/buid"));
+  //Express will serve up index.html
+  //if it doesn't recognize the route
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.res(__dirname, "client", "build", "index.html"));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT);
