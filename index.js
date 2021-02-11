676e62f86ffe788d4032e0ba1e68a8fdc7dfd2cd
xiaodan cui <cxd@xiaodan-MacBook-Pro.local>
2021-02-11 09:54:29 -0800

    modified

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ if (process.env.NODE_ENV === "production") {
   //Express will serve up production assets
   //like main.js,main css
 
-  app.use(express.static("client/buid"));
+  app.use(express.static("client/build"));
   //Express will serve up index.html
   //if it doesn't recognize the route
   const path = require("path");
